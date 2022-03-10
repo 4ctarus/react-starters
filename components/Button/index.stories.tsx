32658c68364d24076a4a036bd790756853f8c5d0
@hhemaidia <hatim.hemaidia@partners-finances.com>
2022-03-10 15:44:18 +0100

    feat() updatebutton doc

--- components/Button/index.stories.tsx
+++ components/Button/index.stories.tsx
@@ -1,9 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import { MdAlarmAdd } from 'react-icons/md';
-import Button, { ButtonColor, ButtonSize } from '.';
-import { defaultTheme } from '../../themes';
-import HStack from '../HStack';
+import Button from '.';
 
 const icons = { 0: MdAlarmAdd, 1: null };
 
@@ -50,54 +48,15 @@ export default {
       },
       defaultValue: null,
     },
-    size: {
-      options: Object.values(ButtonSize),
-      control: {
-        type: 'inline-radio',
-        defaultValue: ButtonSize.NORMAL,
-      },
-      table: {
-        defaultValue: { summary: ButtonSize.NORMAL },
-      },
-    },
-    color: {
-      options: Object.values(ButtonColor),
-      control: {
-        type: 'inline-radio',
-        defaultValue: ButtonColor.PRIMARY,
-      },
-      table: {
-        defaultValue: { summary: ButtonColor.PRIMARY },
-      },
-    },
     onClick: { action: 'onClick' },
   },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => (
-  <HStack className={defaultTheme}>
-    <Button {...args}>
-      <span>Button</span>
-    </Button>
-  </HStack>
+  <Button {...args}>
+    <span>Button</span>
+  </Button>
 );
 
 export const Text = Template.bind({});
-Text.args = {
-  text: true,
-};
-
-export const Outlined = Template.bind({});
-Outlined.args = {
-  outlined: true,
-};
-
-export const Filled = Template.bind({});
-Filled.args = {
-  filled: true,
-};
-
-export const Elevated = Template.bind({});
-Elevated.args = {
-  elevated: true,
-};
+Text.args = {};
